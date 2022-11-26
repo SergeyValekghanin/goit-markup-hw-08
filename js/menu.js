a6b5e4f56f7fe47de664848d78cb2507e348a90d
SergeyValekghanin <72819925+SergeyValekghanin@users.noreply.github.com>
2022-11-26 12:25:36 +0200

    1223

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+(() => {
+    const refs = {
+      openmenuBtn: document.querySelector('[data-menu-open]'),
+      closemenuBtn: document.querySelector('[data-menu-close]'),
+      menu: document.querySelector('[data-menu]'),
+    };
+  
+    refs.openmenuBtn.addEventListener('click', togglemenu);
+    refs.closemenuBtn.addEventListener('click', togglemenu);
+  
+    function togglemenu() {
+      refs.menu.classList.toggle('is-hidden');
+      refs.body.classList.toggle('no-scroll');
+    }
+  })();
+  
